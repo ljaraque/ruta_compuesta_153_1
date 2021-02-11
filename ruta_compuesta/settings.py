cbfa3824aca990551b93018d1a546031a7bc351f
ljaraque <ljaraque@yahoo.com>
2021-02-11 20:28:26 -0300

    se crea modelo de Guitarra y migración y configuramos postgres en Django settings.py

--- ruta_compuesta/settings.py
+++ ruta_compuesta/settings.py
@@ -76,10 +76,23 @@ WSGI_APPLICATION = 'ruta_compuesta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ruta_compuesta_153_1',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
